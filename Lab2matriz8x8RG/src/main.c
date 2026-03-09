c4cd5c885094db0dfb42063180c9e62ff895296d
Kevin <vargaskev2005@gmail.com>
2026-03-09 13:41:38 -0500

    Segunda versión, con movimiento y disparo

--- Lab2matriz8x8RG/src/main.c
+++ Lab2matriz8x8RG/src/main.c
@@ -28,6 +28,7 @@
 #define COL_G7 22
 #define COL_R8 23
 #define COL_G8 25
+int shiplives = 3;
 
 static inline bool btn_pressed(gpio_num_t pin) { //Para revertir la lógica inversa, por la resistencia de pull-up
     if(gpio_get_level(pin) == 0) {
@@ -63,6 +64,20 @@ void turnoff(){
     gpio_set_level(COL_G7, 0);
     gpio_set_level(COL_R8, 0);
     gpio_set_level(COL_G8, 0);
+}
+
+void restart(){
+    turnoff();
+    gpio_set_level(ROW8, 1); //Posición inicial de la nave
+    gpio_set_level(COL_G1, 1);
+    gpio_set_level(ROW1, 1); //Posición inicial en filas de los enemigos
+    gpio_set_level(ROW2, 1);
+    gpio_set_level(ROW3, 1);
+    gpio_set_level(ROW4, 1);
+    gpio_set_level(COL_G3, 1); //Posición inicial en columnas de los enemigos
+    gpio_set_level(COL_G5, 1);
+    gpio_set_level(COL_G7, 1);
+
 }
 
 int column_G(int c){
@@ -154,10 +169,6 @@ int row(int r){
 
 void shipmotion(bool left, bool right){
     int m = 0;
-    gpio_set_level(ROW8, 1); //Posición inicial de la nave
-    gpio_set_level(COL_G1, 1);
-    left = btn_pressed(LEFT);
-    right = btn_pressed(RIGHT);
     for (int i=1; i<=8; i++){
         if (gpio_get_level(column_G(i)) == 1){
             vTaskDelay(pdMS_TO_TICKS(100)); //Antirrebote del botón
@@ -185,30 +196,88 @@ void shipmotion(bool left, bool right){
 void shipattack(){
     for (int i=1; i<=8; i++){
         if (gpio_get_level(column_G(i)) == 1){ //Donde la nave esté
-            for (int j=7; j<=1; j--){
-                if (gpio_get_level(row(j-1)) == 1){ //Hay un enemigo adelante?
-                    gpio_set_level(row(j), 1);
-                    gpio_set_level(column_R(i), 1);
-                    vTaskDelay(pdMS_TO_TICKS(500));
-                    gpio_set_level(row(j), 0); 
-                    gpio_set_level(column_R(i), 0);
-                    vTaskDelay(pdMS_TO_TICKS(250));
-                    gpio_set_level(row(j-1), 0); //Le dio al enemigo
-                } else {
-                    gpio_set_level(row(j), 1);
-                    gpio_set_level(column_R(i), 1); //Dispare
-                    vTaskDelay(pdMS_TO_TICKS(500));
-                }
-                gpio_set_level(row(j), 0);
+            for (int j=7; j>=1; j--){
+                gpio_set_level(row(j), 1);
+                gpio_set_level(column_R(i), 1);
+                vTaskDelay(pdMS_TO_TICKS(500));
+                gpio_set_level(row(j), 0); 
                 gpio_set_level(column_R(i), 0);
+                if (gpio_get_level(row(j-1)) == 1){ //Hay un enemigo adelante?
+                    gpio_set_level(row(j-1), 0); //Le dio al enemigo
+                } 
+                vTaskDelay(pdMS_TO_TICKS(1500)); //Cada 1.5s disparará
             }
         }
-        vTaskDelay(pdMS_TO_TICKS(1500)); //Cada 1.5s disparará
     }
 }
 
 void enemiesmotion(){
-    
+    //Movimiento hacia la derecha:
+    for (int i=1; i<=7; i++){ 
+        for (int j=1; j<=7; j++){
+            if (gpio_get_level(row(j)) == 1 && gpio_get_level(column_G(i)) == 1){
+                gpio_set_level(column_G(i), 0);
+                gpio_set_level(column_G(i+1), 1);
+                vTaskDelay(pdMS_TO_TICKS(300));
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+    //Salto hacia abajo:
+    for (int i=1; i<=7; i++){ 
+        for (int j=7; j>=1; j--){ //Detección de abajo hacia arriba para las filas
+            if (gpio_get_level(row(j)) == 1 && gpio_get_level(column_G(i)) == 1){
+                gpio_set_level(row(j), 0);
+                vTaskDelay(pdMS_TO_TICKS(250));
+                gpio_set_level(row(j+1), 1);
+            }
+        }
+    }
+    //Movimiento hacia la izquierda:
+    for (int i=8; i>=2; i--){ 
+        for (int j=1; j<=7; j++){ 
+            if (gpio_get_level(row(j)) == 1 && gpio_get_level(column_G(i)) == 1){
+                gpio_set_level(column_G(i), 0);
+                gpio_set_level(column_G(i-1), 1);
+                vTaskDelay(pdMS_TO_TICKS(300));
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void enemiesattack(){
+    bool row_detected;
+    for (int j=6; j>=1; j--){  //Detección de abajo hacia arriba para las filas  
+        for (int i=1; i<=7; i++){ 
+            if (gpio_get_level(row(j)) == 1 && gpio_get_level(column_G(i)) == 1){
+                row_detected = true;
+                for (int k=1; k<=7; k++){
+                    gpio_set_level(row(k), 1);
+                    gpio_set_level(column_R(i), 1);
+                    vTaskDelay(pdMS_TO_TICKS(500));
+                    gpio_set_level(row(k), 0); 
+                    gpio_set_level(column_R(i), 0);
+                    if (gpio_get_level(row(k+1)) == 1){ //Hay una nave adelante?
+                        for (int s=1; s<=3; s++){ //Titila cuando le dio a la nave
+                            gpio_set_level(row(k+1), 0); 
+                            vTaskDelay(pdMS_TO_TICKS(250));
+                            gpio_set_level(row(k+1), 1);
+                        }
+                        shiplives--;
+                        if (shiplives == 0){
+                            restart();
+                            shiplives = 3;
+                        }
+                    }
+                }
+            }
+            vTaskDelay(pdMS_TO_TICKS(1500)); //Cada 1.5s disparará
+        }
+        if (row_detected){
+            break; //Para que solo dispare la fila que se encuentra más cercana a la nave
+        }
+    }
 }
 
 void app_main() {
@@ -258,9 +327,15 @@ void app_main() {
     bool moveleft;
     bool moveright;
 
+    restart();
+
     while(1){
+        moveleft = btn_pressed(LEFT);
+        moveright = btn_pressed(RIGHT);
         shipmotion(moveleft, moveright);
         shipattack();
+        enemiesmotion();
+        enemiesattack();
     }
 
 }
