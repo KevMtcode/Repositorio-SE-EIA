04d248427c27d6a0f470db28655c03e71f354596
Kevin <vargaskev2005@gmail.com>
2026-05-26 22:16:38 -0500

    GPIOs a trabajar

--- Lab4_I2C_SPI_BLE/include/pin_map.h
+++ Lab4_I2C_SPI_BLE/include/pin_map.h
@@ -1,1 +1,18 @@
 #pragma once //para evitar que un archivo header sea incluido varias veces
+#define BUZZER (GPIO_NUM_23)
+#define RED (GPIO_NUM_19)
+#define GREEN (GPIO_NUM_18)
+#define BLUE (GPIO_NUM_5)
+
+#define PIN_SS (GPIO_NUM_25)
+#define PIN_SClK (GPIO_NUM_26)
+#define PIN_MOSI (GPIO_NUM_27)
+#define PIN_MISO (GPIO_NUM_14)
+
+#define LCD_SCL (GPIO_NUM_22)
+#define LCD_SDA (GPIO_NUM_21)
+
+#define RTC_SCL (GPIO_NUM_32)
+#define RTC_SDA (GPIO_NUM_33)
+
+#define DEVICE_NAME "PanelHMI"
