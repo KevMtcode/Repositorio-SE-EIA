fa449c04a89b00d0114c58d53188bfad406e0485
Kevin <vargaskev2005@gmail.com>
2026-06-03 22:25:24 -0500

    Comunicación SPI a un RFID para entrada del sistema

--- Lab4_I2C_SPI_BLE/include/spi_rfid_rc522.h
+++ Lab4_I2C_SPI_BLE/include/spi_rfid_rc522.h
@@ -1,8 +1,44 @@
 #pragma once //para evitar que un archivo header sea incluido varias veces
 #include <stdbool.h>
-#define NUM_UIDS 2 //Dos tarjetas
-void spi_init(void);
-void rfid_register(void);
-int memcmp(const void *s1, const void *s2, size_t n); //Para comparación bit por bit
-bool rfid_check_uid(uint8_t *uid);
-bool rfid_card_present(void);
+#include <stdint.h>
+#define NUM_UIDS 2
+
+//Pin Reset del RFID debe siempre estar en 3.3V al estar funcionando
+#define VersionReg 0x37 //para prueba
+
+#define CommandReg       0x01
+#define ComIEnReg        0x02
+#define DivIEnReg        0x03
+#define ComIrqReg        0x04
+#define ErrorReg         0x06
+#define Status1Reg       0x07
+#define Status2Reg       0x08
+#define FIFODataReg      0x09
+#define FIFOLevelReg     0x0A
+#define ControlReg       0x0C
+#define BitFramingReg    0x0D
+#define ModeReg          0x11
+#define TxControlReg     0x14
+#define TxASKReg         0x15
+#define CRCResultRegL    0x22
+#define CRCResultRegH    0x21
+
+#define PCD_IDLE        0x00
+#define PCD_TRANSCEIVE  0x0C
+#define PCD_SOFTRESET   0x0F
+#define PICC_REQA       0x26
+#define PICC_ANTICOLL   0x93
+
+void rfid_write(uint8_t reg, uint8_t dato);
+uint8_t rfid_read(uint8_t reg);
+void rfid_spi_init(void);
+void mfrc522_set_bitmask(uint8_t reg, uint8_t mask);
+void mfrc522_clear_bitmask(uint8_t reg, uint8_t mask);
+void mfrc522_reset(void);
+void mfrc522_antenna_on(void);
+void mfrc522_init(void);
+bool mfrc522_transceive(uint8_t *tx_data, uint8_t tx_len, uint8_t *rx_data, uint8_t *rx_len);
+bool mfrc522_request(uint8_t *atqa);
+bool mfrc522_anticoll(uint8_t *uid);
+bool uid_equals(uint8_t *a, uint8_t *b, uint8_t len);
+bool uid_autorizado(uint8_t *uid);
--- Lab4_I2C_SPI_BLE/src/spi_rfid_rc522.c
+++ Lab4_I2C_SPI_BLE/src/spi_rfid_rc522.c
@@ -1,29 +1,32 @@
 #include "spi_rfid_rc522.h"
 #include "system_lib.h"
-spi_device_handle_t rc522; 
+
+spi_device_handle_t rc522;
 
 uint8_t uids_autorizados[NUM_UIDS][4] = {
-    {0xAA, 0xBB, 0xCC, 0xDD},  // tarjeta 1 — reemplaza con bytes reales
-    {0x11, 0x22, 0x33, 0x44},  // tarjeta 2 — reemplaza con bytes reales
+    {0xEB, 0xF8, 0xEE, 0x1A},  // tarjeta azul — reemplaza con bytes reales
+    {0x37, 0x44, 0x16, 0x17},  // tarjeta blanca — reemplaza con bytes reales
 };
 
-uint8_t num_uids = sizeof(uids_autorizados) / sizeof(uids_autorizados[0]);
-
-static void rfid_write(uint8_t reg, uint8_t dato) { 
+void rfid_write(uint8_t reg, uint8_t dato) { 
     uint8_t buf[2]; 
-    buf[0] = reg & 0x7E;   //AND con 01111110 para garantizar 0 en escritura, 0 en bit 0 por formato y lo demás queda igual
+    buf[0] = (reg << 1) & 0x7E;   //AND con 01111110 para garantizar 0 en escritura, 0 en bit 0 por formato y lo demás queda igual
     buf[1] = dato;  // Tamaño de caracter final, valor del registro, calculado con peso decimal
  
     spi_transaction_t t = { 
         .length    = 16, 
         .tx_buffer = buf, 
     }; 
-    spi_device_transmit(rc522, &t); 
+    esp_err_t ret = spi_device_transmit(rc522, &t);
+    if(ret != ESP_OK)
+    {
+        printf("SPI Error\n");
+    }
 } 
-static uint8_t rfid_read(uint8_t reg) {
+uint8_t rfid_read(uint8_t reg) {
     uint8_t buf_tx[2];
     uint8_t buf_rx[2];
-    buf_tx[0] = (reg | 0x80) & 0xFE;; //OR con 10000000 y  AND con 11111110
+    buf_tx[0] =  ((reg << 1) & 0x7E) | 0x80; //OR con 10000000 y  AND con 11111110
     buf_tx[1] = 0x00; // byte dummy para generar el clock
 
     spi_transaction_t t = {
@@ -31,8 +34,12 @@ static uint8_t rfid_read(uint8_t reg) {
         .tx_buffer = buf_tx,
         .rx_buffer = buf_rx,  // aquí recibes la respuesta
     };
-
-    spi_device_transmit(rc522, &t);
+    
+    esp_err_t ret = spi_device_transmit(rc522, &t);
+    if(ret != ESP_OK)
+    {
+        printf("SPI Error\n");
+    }
     return buf_rx[1];
 }
 
@@ -40,7 +47,7 @@ void rfid_spi_init(void){
     spi_bus_config_t bus = { 
         .mosi_io_num   = PIN_MOSI, 
         .miso_io_num   = PIN_MISO, 
-        .sclk_io_num   = PIN_SClK, 
+        .sclk_io_num   = PIN_SCLK, 
         .quadwp_io_num = -1, 
         .quadhd_io_num = -1, 
     }; 
@@ -48,7 +55,7 @@ void rfid_spi_init(void){
 
     // Agregar el RFID como dispositivo SPI 
     spi_device_interface_config_t dev = { 
-        .clock_speed_hz = 1000000,  // 1 MHz 
+        .clock_speed_hz = 100000,  // 100kHz 
         .mode           = 0,        // CPOL=0, CPHA=0 
         .spics_io_num   = PIN_SS, 
         .queue_size     = 1, 
@@ -57,82 +64,194 @@ void rfid_spi_init(void){
 
 }
 
-void rfid_register(void){
-    // SoftReset:
-    rfid_write(0x01, 0x0F);
-    vTaskDelay(pdMS_TO_TICKS(50)); // espera que el reset termine
+void mfrc522_set_bitmask(uint8_t reg, uint8_t mask){
+    uint8_t tmp = rfid_read(reg);
 
-    // Timer (para que los comandos no queden colgados)
-    rfid_write(0x2A, 0x80); //bit 7 TAuto, 128 de peso en decimal --> 0x80
-    rfid_write(0x2B, 0xA9); 
-    rfid_write(0x2C, 0x03); //TReloadReg
-    rfid_write(0x2D, 0XE8);
-
-    // Forzar 100% ASK:
-    rfid_write(0x15, 0x40); // bit 6  --> 64 en decimal --> 0x40
-
-    // Modo CRC:
-    rfid_write(0x11, 0x01); // 6363 para ISO 14443A
-
-    // Encender la antena
-    rfid_write(0x14, 0x03); //bits 0 y 1 --> 1 + 2 en decimal --> 0x03
+    rfid_write(reg,
+                      tmp | mask);
 }
 
-void rfid_read_uid(uint8_t *uid) {
+void mfrc522_clear_bitmask(uint8_t reg, uint8_t mask){ //apagar bits
+    uint8_t tmp = rfid_read(reg);
 
-    rfid_write(0x01, 0x00); // Idle command
+    rfid_write(reg,
+                      tmp & (~mask));
+}
 
-    // Limpiar el FIFO:
-    rfid_write(0x0A, 0x80);
+void mfrc522_reset(void){
+    rfid_write(CommandReg,
+                      PCD_SOFTRESET);
 
-    // Escribir el comando REQA en el FIFO
-    rfid_write(0x09, 0x26);
+    vTaskDelay(pdMS_TO_TICKS(50));
+}
 
-    // Configurar BitFramingReg para enviar 7 bits y activar StartSend
-    rfid_write(0x0D, 0x87); //StartSend y TxLasBits --> bit 7 + 2 a 0 bit
+void mfrc522_antenna_on(void){
+    uint8_t value =
+        rfid_read(TxControlReg);
 
-    // Activar comando Transceive en CommandReg:
-    rfid_write(0x01, 0x0C);
-
-    // Esperar respuesta de la tarjeta:
-    // Busca en ComIrqReg (0x04) los bits RxIRq e IdleIRq
-    // El loop termina cuando alguno de esos bits se activa
-    uint8_t irq;
-    do {
-        irq = rfid_read(0x04);
-    } while ((irq & 0x30) == 0); //Solo si RxIrq y IdleIrq están activados --> 0x30 = 00110000 
-
-    // Leer cuántos bytes llegaron al FIFO:
-    uint8_t len = rfid_read(0x0A);
-
-    // PASO 8: Leer los bytes del FIFO uno por uno
-    for (uint8_t i = 0; i < len; i++) {
-        uid[i] = rfid_read(0x09); 
+    if (!(value & 0x03))
+    {
+        rfid_write(
+            TxControlReg,
+            value | 0x03);
     }
 }
 
-bool rfid_check_uid(uint8_t *uid) {
-    for (uint8_t i = 0; i < NUM_UIDS; i++) {
-        if (memcmp(uid, uids_autorizados[i], 4) == 0) { //es cero si ambos UIDs son iguales
+void mfrc522_init(void){
+    mfrc522_reset();
+
+    rfid_write(0x2A, 0x8D); // TModeReg
+    rfid_write(0x2B, 0x3E); // TPrescalerReg
+    rfid_write(0x2D, 30);   // TReloadRegL
+    rfid_write(0x2C, 0);    // TReloadRegH
+
+    rfid_write(TxASKReg, 0x40);
+    rfid_write(ModeReg, 0x3D);
+
+    mfrc522_antenna_on();
+
+    //Comprobar:
+    /*printf("ModeReg = 0x%02X\n", rfid_read(ModeReg)); //Aparece 0x3D
+    printf("TxASKReg = 0x%02X\n", rfid_read(TxASKReg)); //Aparece 0X40
+    printf("TxControlReg = 0x%02X\n", rfid_read(TxControlReg)); //0x#3, terminando en 3 significa que los bits 0 y 1 están activados para la antena
+    */   
+}
+
+bool mfrc522_transceive(uint8_t *tx_data, uint8_t tx_len, uint8_t *rx_data, uint8_t *rx_len){
+    uint8_t irq;
+    uint16_t timeout = 2000;
+
+    rfid_write(ComIrqReg, 0x7F); //Limpiar interrupciones con 01111111
+
+    rfid_write(FIFOLevelReg, 0x80); //Vaciar FIFO con FlushBuffer
+
+    rfid_write(CommandReg, PCD_IDLE); //Detener comando anterior
+
+    for(uint8_t i = 0; i < tx_len; i++){ //Cargar FIFO
+        rfid_write(FIFODataReg, tx_data[i]);
+    }
+
+    rfid_write(CommandReg, PCD_TRANSCEIVE); //Lanzar transacción
+
+    mfrc522_set_bitmask(BitFramingReg, 0x80); //StartSend (bit 7)
+
+    while(timeout--){   //Esperar respuesta
+        irq = rfid_read(ComIrqReg);
+
+       if (irq & 0x20){ // RxIRq = SOLO recepción válida
+            break;
+        }
+        if (irq & 0x01){ // TimerIRq = timeout real
+            return false;
+        }
+        vTaskDelay(pdMS_TO_TICKS(1));
+    }
+    printf("IRQ = 0x%02X\n", irq); //Si es 0x20, hay recepción; si es 0x01, timeout
+
+    mfrc522_clear_bitmask(BitFramingReg, 0x80); //Apagar StartSend
+
+    if(timeout == 0){
+        return false;
+    }
+    if(irq & 0x01){
+        printf("TIMEOUT\n");
+        return false;
+    }
+
+    //Revisar errores:
+    uint8_t error = rfid_read(ErrorReg);
+
+    if(error & 0X02){
+        return false;
+    }
+
+    uint8_t count = rfid_read(FIFOLevelReg); //Número de bytes recibidos
+    *rx_len = count;
+
+    for(uint8_t i = 0; i < count; i++){ //Leer FIFO
+        rx_data[i] = rfid_read(FIFODataReg);
+    }
+    rfid_write(BitFramingReg, 0x00);
+    return true;
+}
+
+bool mfrc522_request(uint8_t *atqa){
+    uint8_t reqa = PICC_REQA;
+    uint8_t len;
+
+    // REQA usa 7 bits
+    rfid_write(BitFramingReg, 0x07);
+
+    return mfrc522_transceive(
+        &reqa,
+        1,
+        atqa,
+        &len
+    );
+}
+
+bool mfrc522_anticoll(uint8_t *uid){
+    uint8_t cmd[2];
+    uint8_t len;
+
+    cmd[0] = PICC_ANTICOLL;
+    cmd[1] = 0x20;
+
+    rfid_write(BitFramingReg, 0x00);
+
+    if(!mfrc522_transceive(
+            cmd,
+            2,
+            uid,
+            &len))
+    {
+        return false;
+    }
+
+    printf("UID response (%d bytes): ", len);
+
+    for(uint8_t i = 0; i < len; i++)
+    {
+        printf("%02X ", uid[i]);
+    }
+
+    printf("\n");
+
+    // Anticollision nivel 1 debe devolver:
+    // UID0 UID1 UID2 UID3 BCC
+    if(len != 5)
+    {
+        return false;
+    }
+
+    uint8_t bcc =
+        uid[0] ^
+        uid[1] ^
+        uid[2] ^
+        uid[3];
+
+    if(bcc != uid[4])
+    {
+        printf("BCC error\n");
+        return false;
+    }
+
+    return true;
+}
+
+bool uid_equals(uint8_t *a, uint8_t *b, uint8_t len){ //Comparación de UID
+    for (int i = 0; i < len; i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
+bool uid_autorizado(uint8_t *uid){ //Validación
+    for (int i = 0; i < NUM_UIDS; i++) {
+        if (uid_equals(uid, uids_autorizados[i], 4)) {
             return true;
         }
     }
     return false;
 }
 
-bool rfid_card_present(void) { // Detecta si hay una tarjeta presente
-    rfid_write(0x01, 0x00);
-    rfid_write(0x0A, 0x80);
-    rfid_write(0x09, 0x26);
-    rfid_write(0x0D, 0x87);
-    rfid_write(0x01, 0x0C);
-
-    uint8_t irq;
-    do {
-        irq = rfid_read(0x04);
-    } while ((irq & 0x30) == 0);
-
-    // Leer ErrorReg y verificar:
-    uint8_t error = rfid_read(0x06);
-    return (error & 0x08) == 0; // sin error = tarjeta presente
-}
